8fdbd3c03fb1c79aa0226290b3c554cb21ab9430
eljily <eljilysidibrahim@gmail.com>
2024-08-19 15:41:14 +0000

    Added lombok annotations

--- src/main/java/com/sidibrahim/oms/entity/Order.java
+++ src/main/java/com/sidibrahim/oms/entity/Order.java
@@ -1,12 +1,20 @@
 package com.sidibrahim.oms.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
