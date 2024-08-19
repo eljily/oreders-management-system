6cf53c0f61bc18a762d605d20ee9161b8df7674a
eljily <eljilysidibrahim@gmail.com>
2024-08-19 13:20:16 +0000

    Project kickoff

---
+++ src/main/java/com/sidibrahim/restomanagementsystem/RestoManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sidibrahim.restomanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestoManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestoManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sidibrahim/restomanagementsystem/RestoManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidibrahim.restomanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
