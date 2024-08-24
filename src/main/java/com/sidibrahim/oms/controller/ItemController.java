f4132c8bfcb403e5937a0f1e2d221e75e4c9f045
eljily <eljilysidibrahim@gmail.com>
2024-08-24 12:52:43 +0000

    added dummy endpoints in the itemController

--- src/main/java/com/sidibrahim/oms/controller/ItemController.java
+++ src/main/java/com/sidibrahim/oms/controller/ItemController.java
@@ -1,4 +1,36 @@
 package com.sidibrahim.oms.controller;
 
+import com.sidibrahim.oms.entity.Item;
+import com.sidibrahim.oms.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController("/api/items")
+@RequiredArgsConstructor
 public class ItemController {
+    private final ItemService itemService;
+
+    @PostMapping
+    public ResponseEntity<Item> save(@RequestBody Item item) {
+        return ResponseEntity.ok(itemService.save(item));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Item> getById(Integer id) {
+        return ResponseEntity.ok(itemService.findById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(Integer id) {
+        itemService.deleteById(id);
+        return ResponseEntity.ok(200);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Item>> getAll() {
+        return ResponseEntity.ok(itemService.findAll());
+    }
 }
--- src/main/java/com/sidibrahim/oms/service/ItemService.java
+++ src/main/java/com/sidibrahim/oms/service/ItemService.java
@@ -1,4 +1,27 @@
 package com.sidibrahim.oms.service;
 
+import com.sidibrahim.oms.entity.Item;
+import com.sidibrahim.oms.repository.ItemRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
 public class ItemService {
+    private final ItemRepository itemRepository;
+
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+    public Item save(Item item){
+        return itemRepository.save(item);
+    }
+    public Item findById(Integer id){
+        return itemRepository.findById(id).orElse(null);
+    }
+    public List<Item> findAll(){
+        return itemRepository.findAll();
+    }
+    public void deleteById(Integer id){
+        itemRepository.deleteById(id);
+    }
 }
